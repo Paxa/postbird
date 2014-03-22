0d91a20c3da9f9729e7636776a9473a872b2b402
Pavel <pavel.evst@gmail.com>
2014-03-22 13:51:41 +0700

    Update build script

--- build.sh
+++ build.sh
@@ -2,18 +2,38 @@
 
 echo "This script requires node-webkit.app in your applications folder"
 
+TARGET_DIR="${HOME}/postbird.app"
+APP_TARGET_DIR="${TARGET_DIR}/Contents/Resources/app.nw"
+echo $TARGET_DIR
+
+
+if [ -e "$TARGET_DIR" ]; then
+  echo "Removing existing build"
+  rm -rf $TARGET_DIR
+fi
+
 #read -p "Is node-webkit.app in your applications folder (y/n)? " -n 1 -r
 #if [[ $REPLY =~ ^[Yy]$ ]]
 #then
     # node-webkit exists continue build
-    cp -r /Applications/node-webkit.app ~/postbird.app
+    cp -r /Applications/node-webkit.app ${TARGET_DIR}
 
     # create app.nw and move to correct location
-    cp -r . ~/postbird.app/Contents/Resources/app.nw
+    git checkout-index -a --prefix="${APP_TARGET_DIR}/"
+    #cp -r . ~/postbird.app/Contents/Resources/app.nw
+
+    rm ${APP_TARGET_DIR}/build.sh
+    rm ${APP_TARGET_DIR}/pg.icns
+    rm ${APP_TARGET_DIR}/pg.png
+    rm ${APP_TARGET_DIR}/info.plist
+    rm -rf ${APP_TARGET_DIR}/notifier.app
+    
 
     # copy icon and plist file
-    cp ./pg.icns ~/postbird.app/Contents/Resources
-    cp ./info.plist ~/postbird.app/Contents
+    cp ./pg.icns ${TARGET_DIR}/Contents/Resources
+    cp ./info.plist ${TARGET_DIR}/Contents
+
+    cp ./pg.icns ${TARGET_DIR}/Contents/Resources
 
     # mv postbird.app ../../bin/
 
