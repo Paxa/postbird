7b8a1eac6c56a46fe104ef07f330b8717f459837
Pavel <pavel.evst@gmail.com>
2014-04-21 02:17:11 +0700

    fix sweet loader

--- sugar/sweet-loader.js
+++ sugar/sweet-loader.js
@@ -1,11 +1,11 @@
 sweet = require('sweet.js');
 
-sweet.loadMacro('./sugar/macros/str');
-sweet.loadMacro('./sugar/macros/puts');
-sweet.loadMacro('./sugar/macros/unless');
+sweet.loadMacro(__dirname + '/macros/str');
+sweet.loadMacro(__dirname + '/macros/puts');
+sweet.loadMacro(__dirname + '/macros/unless');
 //sweet.loadMacro('./sugar/macros/in');
 //sweet.loadMacro('./sugar/macros/prototype'); // -> this two don't work with my hack
-sweet.loadMacro('./sugar/macros/nullity');
-sweet.loadMacro('./sugar/macros/lambda');
+sweet.loadMacro(__dirname + '/macros/nullity');
+sweet.loadMacro(__dirname + '/macros/lambda');
 
 //require('./test.sjs');
