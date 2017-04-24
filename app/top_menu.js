83bf05c9a01f38f5cd20566ae34dffacbc7a1518
Pavel <pavel.evst@gmail.com>
2017-04-25 00:10:32 +0800

    Use editMenu for top menu

--- app/top_menu.js
+++ app/top_menu.js
@@ -35,41 +35,7 @@ var template = [
   },
   {
     label: 'Edit',
-    submenu: [
-      {
-        label: 'Undo',
-        accelerator: 'CmdOrCtrl+Z',
-        role: 'undo'
-      },
-      {
-        label: 'Redo',
-        accelerator: 'Shift+CmdOrCtrl+Z',
-        role: 'redo'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Cut',
-        accelerator: 'CmdOrCtrl+X',
-        role: 'cut'
-      },
-      {
-        label: 'Copy',
-        accelerator: 'CmdOrCtrl+C',
-        role: 'copy'
-      },
-      {
-        label: 'Paste',
-        accelerator: 'CmdOrCtrl+V',
-        role: 'paste'
-      },
-      {
-        label: 'Select All',
-        accelerator: 'CmdOrCtrl+A',
-        role: 'selectall'
-      },
-    ]
+    role: 'editMenu'
   },
   {
     label: 'Database',
@@ -168,16 +134,12 @@ var template = [
       },
       {
         label: 'Zoom In',
-        click: () => {
-          webFrame.setZoomFactor(webFrame.getZoomFactor() + 0.3);
-        },
+        role: 'zoomin',
         accelerator: 'CmdOrCtrl+='
       },
       {
         label: 'Zoom Out',
-        click: () => {
-          webFrame.setZoomFactor(webFrame.getZoomFactor() - 0.3);
-        },
+        role: 'zoomout',
         accelerator: 'CmdOrCtrl+-'
       },
       {
