4700e8b99eb82a1c826056fec128fbc1b9a569ca
saylestyler <saylestyler@gmail.com>
2019-05-30 23:35:57 -0400

    syntax updates / consolidation

--- app/views/dialogs/new_snippet.js
+++ app/views/dialogs/new_snippet.js
@@ -1,52 +1,52 @@
-var fs = require('fs');
-var path = require('path');
-//var electron = require('electron');
+const fs = require('fs');
+const path = require('path');
 
 class NewSnippet extends DialogBase {
   /*::
   code: string
   */
 
-  constructor (code) {
+  constructor(code) {
     super(null, {
-      dialogClass: "new-snippet-dialog",
-      title: "New Snippet",
+      dialogClass: 'new-snippet-dialog',
+      title: 'New Snippet',
       code: code
     });
+
     this.showWindow();
   }
 
-  showWindow () {
-    var nodes = App.renderView('dialogs/new_snippet', {code: this.code});
+  showWindow() {
+    const nodes = App.renderView('dialogs/new_snippet', { code: this.code });
     this.content = this.renderWindow(this.title, nodes);
     window.hljs.highlightBlock(this.content.find('code')[0]);
 
     this.bindFormSubmitting();
   }
 
-  onSubmit (data) {
-    // Apped to json file
-    if (data.snippet_name) {
-      var snippetsPath = path.join(electron.remote.app.getPath('userData'), 'custom_snippets.json');
-
-      var customSnippets = {};
-      if (fs.existsSync(snippetsPath)) {
-        customSnippets = JSON.parse(fs.readFileSync(snippetsPath))
-      }
-
-      customSnippets[data.snippet_name] = {
-        description: data.description,
-        sql: this.code
-      };
-
-      console.log('saving snippet to ', snippetsPath);
-      console.log('data', JSON.stringify(customSnippets, null, 2));
-      fs.writeFileSync(snippetsPath, JSON.stringify(customSnippets, null, 2));
-      this.close();
+  onSubmit(data) {
+    if (!data.snippet_name) {
+      return $u.alert('A Snippet name is required!');
     }
-    else {
-      $u.alert('A Snippet name is required!');
+
+    const snippetsPath = path.join(electron.remote.app.getPath('userData'), 'custom_snippets.json');
+
+    let customSnippets = {};
+
+    if (fs.existsSync(snippetsPath)) {
+      customSnippets = JSON.parse(fs.readFileSync(snippetsPath))
     }
+
+    customSnippets[data.snippet_name] = {
+      description: data.description,
+      sql: this.code
+    };
+
+    console.log(`${data.snippet_name} saved to ${snippetsPath}`);
+
+    fs.writeFileSync(snippetsPath, JSON.stringify(customSnippets, null, 2));
+
+    this.close();
   }
 }
 
